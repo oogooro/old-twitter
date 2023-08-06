7017e8fb9f76cb2263e6b10c13da4421c57c26e8
oogooro <70372978+oogooro@users.noreply.github.com>
2023-08-06 21:24:36 +0200

    Add comment

--- scripts/content.js
+++ scripts/content.js
@@ -52,5 +52,6 @@ const titleObserver = new MutationObserver((mutationList) => {
 
 waitForElement('title').then(title => titleObserver.observe(title, { childList: true, }));
 
+// Change "Tweet" buttons
 waitForElement('[data-testid="tweetButtonInline"]>div>span>span').then(element => element.innerText = 'Tweet');
 waitForElement('[data-testid="SideNav_NewTweet_Button"]>div>span>div>div>span>span').then(element => element.innerText = 'Tweet');
